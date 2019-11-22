d4c95c6657cda0dc37eb1985e83cc2ccaa0ef710
tsubasa.komatsu <negi19860323283@gmail.com>
2019-11-22 15:48:59 +0000

    add kadaitask

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,6 +44,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request,[
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+            ]);
+            
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -90,6 +95,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request,[
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+            ]);
+            
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
